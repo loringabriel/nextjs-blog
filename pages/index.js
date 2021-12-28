8b005eaae61348d1b9aedd57806cf47a83c378c2
Lorin Muller <loringabriel@gmail.com>
2021-12-28 09:41:17 +0200

    added profile picture, name and description

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Full-Stack Software Engineer mostly interested in Frontend and Cloud based technologies.</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
