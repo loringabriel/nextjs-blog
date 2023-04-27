4b90fad65a3055f97d1dac499645c5afc472f4e2
loringabriel <loringabriel@gmail.com>
2023-04-27 11:24:00 +0300

    test cookiebot

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,44 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
 
 export default function Home({
-  allPostsData
+  allPostsData,
 }: {
   allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
+    date: string;
+    title: string;
+    id: string;
+  }[];
 }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
+        <script
+          id="Cookiebot"
+          data-culture="EN"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="079feda4-1474-4412-b9f6-4e9796d9b28b"
+          type="text/javascript"
+        ></script>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Full-Stack Software Engineer mostly interested in Frontend and Cloud based technologies.</p>
+        <p>
+          Full-Stack Software Engineer mostly interested in Frontend and Cloud
+          based technologies.
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
@@ -40,14 +48,14 @@ export default function Home({
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
-}
+      allPostsData,
+    },
+  };
+};
