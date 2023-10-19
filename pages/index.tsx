f6a860ace7776fb3a9f226857d08fc19be22234a
Lorin Muller <loringabriel@gmail.com>
2023-10-19 18:56:42 +0300

    test mailchimp

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,7 @@ export default function Home({
           data-cbid="079feda4-1474-4412-b9f6-4e9796d9b28b"
           type="text/javascript"
         ></script> */}
+        <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/c8bd633a616222dbc7a990d8e/b254fb88c5d1d95a11cb821fb.js");</script>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
